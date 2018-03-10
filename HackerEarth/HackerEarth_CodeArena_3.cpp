1cd378ee374b0be72dc3dea73d02801c8a851088
DollarAkshay <akshay95aradhya@gmail.com>
2018-03-10 21:42:08 +0530

    CodeArena 3rd Victory :grin:

---
+++ HackerEarth/HackerEarth_CodeArena_3.cpp
@@ -0,0 +1,69 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ *  $DollarAkshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerearth.com/codearena/ring/2962ac5/
+
+#include <algorithm>
+#include <assert.h>
+#include <ctype.h>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORD(i, a, b) for (int i = a; i >= b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MSX(a, x) memset(a, x, sizeof(a))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+int a[1000000];
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+	REP(i, n) {
+		scanf("%d", &a[i]);
+	}
+
+	REP(i, n) {
+		bool isPrime = true;
+		REP(j, n) {
+			if (i == j) {
+				continue;
+			}
+			if (a[i] % a[j] == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime) {
+			printf("%d ", a[i]);
+		}
+	}
+	return 0;
+}
+
+//
