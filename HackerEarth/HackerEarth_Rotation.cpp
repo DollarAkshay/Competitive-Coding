a36179285bd9840196b1107cc260485743811ec8
DollarAkshay <akshay95aradhya@gmail.com>
2019-06-22 10:09:40 +0530

    Solved one problem on HackerEarth

---
+++ HackerEarth/HackerEarth_Rotation.cpp
@@ -0,0 +1,63 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ *  $DollarAkshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//https://www.hackerearth.com/challenges/competitive/june-circuits-19/algorithm/rotation-1-38ecf5a7/
+
+#pragma GCC optimize("O3", "unroll-loops", "omit-frame-pointer", "inline")
+
+#include <algorithm>
+#include <assert.h>
+#include <chrono>
+#include <ctype.h>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORD(i, a, b) for (int i = a; i >= b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define DB(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
+#define MSX(a, x) memset(a, x, sizeof(a))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+char a[1001];
+char b[1001];
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+	scanf("%s\n%s", a, b);
+
+	REP(i, n) {
+		if (strstr(b, a + i)) {
+			printf("%d\n", i);
+			break;
+		}
+	}
+	return 0;
+}
+
+//Solved
