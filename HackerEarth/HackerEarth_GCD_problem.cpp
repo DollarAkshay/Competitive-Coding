a9c148671d3414934b4ccfd40a9bb81ad1771dd9
DollarAkshay <akshay95aradhya@gmail.com>
2019-06-25 13:13:48 +0530

    Finaly. Fucking mobius functions :D

--- HackerEarth/HackerEarth_GCD_problem.cpp
+++ HackerEarth/HackerEarth_GCD_problem.cpp
@@ -6,7 +6,7 @@
 
 //https://www.hackerearth.com/challenges/competitive/june-circuits-19/algorithm/calculate-gcd-june-circuit-e5e41856/
 
-#pragma GCC optimize("O3", "unroll-loops", "omit-frame-pointer", "inline")
+// #pragma GCC optimize("O3", "unroll-loops", "omit-frame-pointer", "inline")
 
 #include <algorithm>
 #include <assert.h>
@@ -32,17 +32,87 @@ using namespace std::chrono;
 
 #define sp system("pause")
 #define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORS(i, a, b, s) for (int i = a; i <= b; i += s)
 #define FORD(i, a, b) for (int i = a; i >= b; --i)
 #define REP(i, n) FOR(i, 0, (int)n - 1)
 #define DB(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
-#define MSX(a, x) memset(a, x, sizeof(a))
+#define FILL(a, x) fill(begin(a), end(a), x)
 #define SORT(a, n) sort(begin(a), begin(a) + n)
 #define REV(a, n) reverse(begin(a), begin(a) + n)
 #define ll long long
 #define pii pair<int, int>
 #define MOD 1000000007
+#define MAX 1000000
 
-int a[1000000];
+int a[MAX];
+
+int p[MAX + 1];
+int m[MAX + 1];
+ll int mi[MAX + 1];
+
+ll int fastPowMod(ll int b, ll int e) {
+
+	if (e == 0) {
+		return 1;
+	}
+	else if (e % 2 == 1) {
+		return fastPowMod(b, e - 1) * b % MOD;
+	}
+	else {
+		ll int res = fastPowMod(b, e / 2);
+		return (res * res) % MOD;
+	}
+}
+
+int primeSeive() {
+	FILL(p, 1);
+	p[0] = p[1] = 0;
+
+	int limit = sqrt(MAX);
+	FOR(i, 2, limit) {
+		if (p[i] == 1) {
+			FORS(j, i * i, MAX, i) {
+				p[j] = 0;
+			}
+		}
+	}
+}
+
+int mobiusSeive() {
+
+	FILL(m, 1);
+	m[0] = 0;
+	m[1] = 1;
+
+	int limit = sqrt(MAX);
+
+	// Calculate +1/-1 Mobius values
+	FOR(i, 2, MAX) {
+		if (p[i] == 1) {
+			FORS(j, i, MAX, i) {
+				m[j] *= -1;
+			}
+		}
+	}
+
+	// Set all prime squares to 0
+	FOR(i, 2, limit) {
+		if (p[i] == 1) {
+			FORS(j, i * i, MAX, i * i) {
+				m[j] = 0;
+			}
+		}
+	}
+}
+
+int mobiusInvSeive() {
+	FILL(mi, 0);
+	FOR(i, 1, MAX) {
+		FORS(j, i, MAX, i) {
+			mi[j] = (mi[j] + fastPowMod(i, 4) * m[j / i] % MOD) % MOD;
+		}
+	}
+}
 
 ll int inverse_mod(ll int a, ll int p) {
 	ll int ne = 1, old = 0, q = p, r, h;
@@ -70,29 +140,11 @@ ll int ncrMod(int n, int r) {
 	return res;
 }
 
-ll int fastPowMod(ll int b, ll int e) {
-
-	if (e == 0) {
-		return 1;
-	}
-	else if (e % 2 == 1) {
-		return fastPowMod(b, e - 1) * b % MOD;
-	}
-	else {
-		ll int res = fastPowMod(b, e / 2);
-		return (res * res) % MOD;
-	}
-}
-
 ll int gcd(ll int a, ll int b) {
 	if (a == 0) {
 		return b;
 	}
 	return gcd(b % a, a);
-}
-
-ll int lcm(ll int a, ll int b) {
-	return a * b / gcd(a, b);
 }
 
 ll int bruteforce(int n) {
@@ -115,10 +167,10 @@ ll int bruteforce(int n) {
 	// Grpuped by GCD Value
 	FOR(g, 1, n / 4) {
 		printf("GCD = %d\n", g);
-		REP(i, min(35, (int)havingGCD[g].size())) {
+		REP(i, min(5, (int)havingGCD[g].size())) {
 			printf("%2d %2d %2d %2d\n", havingGCD[g][i][0], havingGCD[g][i][1], havingGCD[g][i][2], havingGCD[g][i][3]);
 		}
-		if (havingGCD[g].size() > 35) {
+		if (havingGCD[g].size() > 5) {
 			printf("...\n");
 		}
 	}
@@ -131,22 +183,7 @@ ll int algo(int n) {
 	ll int res = 0;
 
 	FOR(i, 1, n / 4) {
-		ll int x = 2 * i;
-		ll int k1 = (n - x) / i;
-		ll int k2 = k1 - 1;
-		ll int k3 = k1 - 2;
-
-		ll int k1Count = ((k1 * k1 * k1 - k1) / 6) % MOD;
-		ll int k2Count = ((k2 * k2 * k2 - k2) / 6) % MOD;
-		ll int k3Count = ((k3 * k3 * k3 - k3) / 6) % MOD;
-
-		ll int k1Res = fastPowMod(i, 4) * k1Count % MOD;
-		ll int k2Res = fastPowMod(i, 4) * k2Count % MOD;
-		ll int k3Res = fastPowMod(i, 4) * k3Count % MOD;
-
-		res = (res + k1Res + k2Res + k3Res) % MOD;
-
-		printf(" GCD = %2d | %7lld | %7lld + %7lld + %7lld = %7lld\n", i, ncrMod(n / i, 4), k1Count, k2Count, k3Count, k1Count + k2Count + k3Count);
+		res = (res + mi[i] * ncrMod(n / i, 4) % MOD) % MOD;
 	}
 
 	return res;
@@ -154,17 +191,22 @@ ll int algo(int n) {
 
 int main() {
 
-	int t = 1;
-	// scanf("%d", &t);
+	primeSeive();
+	mobiusSeive();
+	mobiusInvSeive();
+
+	int t;
+	scanf("%d", &t);
 	REP(tc, t) {
 		int n;
 		scanf("%d", &n);
 
-		ll int bfRes = bruteforce(n);
-		ll int algoRes = algo(n);
-		printf("\nBF   = %lld\n", bfRes);
-		printf("ALGO = %lld\n", algoRes);
-		printf("Diff = %lld\n", bfRes - algoRes);
+		// ll int bfRes = bruteforce(n);
+		// printf("\nBF   = %lld\n", bfRes);
+
+		ll int res = algo(n);
+		assert(res > 0);
+		printf("%lld\n", res);
 	}
 	return 0;
 }
